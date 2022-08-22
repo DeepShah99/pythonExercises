3fd274efde8c53a1627197176b04ca1a6f1c2369
Deep Shah <deepishah1999@gmail.com>
2022-08-22 17:45:42 -0400

    LL prepend

---
+++ LLprepend.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+    
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+    
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+my_ll = LinkedList(4)
+my_ll.prepend(5)
+my_ll.prepend(6)
+my_ll.print_list()
