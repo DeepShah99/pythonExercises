59ed3e9f20857085572ebed3667c30cf9abb112c
Deep Shah <deepishah1999@gmail.com>
2022-09-18 20:40:51 -0400

    BST

---
+++ BSTConstructor.py
@@ -0,0 +1,16 @@
+from optparse import Values
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+
+BST = BinarySearchTree()
+print(BST.root)
